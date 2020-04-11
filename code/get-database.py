47c19c44671ac0014927c8fa441704a604881606
Ludivine Lacour <ludivinelacour.contact@gmail.com>
2020-04-11 22:43:25 +0200

    update get-database.py

--- code/get-database.py
+++ code/get-database.py
@@ -54,47 +54,46 @@ def get_specialities():
     return list_of_specialities_slug, list_of_specialities
 
 
-def get_database(specialties_slug=[]):
+def get_database(specialities_slug=[]):
     
     # Final scrapping for all specialities
     base_url='https://www.doctolib.fr'
     location='france'
-    speciality='stomatologue'
+    #speciality='stomatologue'
     df_final=pd.DataFrame()
     
-    ## Comment of the part for big big scrapping
-    #for i in range(len(specialties_slug)):
+    for i in range(len(specialities_slug)):
         
-    #    speciality=specialities_slug[i]
-    page_num=0
-    doctors=True
-    print("speciality: ", speciality, " location: ", location)
+        speciality=specialities_slug[i]
+        page_num=0
+        doctors=True
+        print("speciality: ", speciality, " location: ", location)
+        
+        while doctors:
+        
+            page_num+=1
     
-    while doctors:
+            final_url=f"{base_url}/{speciality}/{location}?page={page_num}"
     
-        page_num+=1
-
-        final_url=f"{base_url}/{speciality}/{location}?page={page_num}"
-
-        result=get_json(final_url, speciality, location, page_num)
-        data_doctors=result['data']['doctors']
-        data_directory=result['data']['directory_doctors']
-        #variable doctors allows to set the while loop to False when there is no more results
-        doctors=(len(data_doctors)!=0) or (len(data_directory)!=0)
-
-        if len(data_doctors)>0:
-            data=pd.json_normalize(data_doctors)
-            data['doctolib_profile']=True
-            df_final=df_final.append(data)
-
-        if len(data_directory)>0:
-            data=pd.json_normalize(data_directory)
-            data=data.rename(columns={'zip_code':'zipcode'})
-            data['doctolib_profile']=False
-            data['profile_id']=-1
-            df_final=df_final.append(data)
-
-        time.sleep(0.5)
+            result=get_json(final_url, speciality, location, page_num)
+            data_doctors=result['data']['doctors']
+            data_directory=result['data']['directory_doctors']
+            #variable doctors allows to set the while loop to False when there is no more results
+            doctors=(len(data_doctors)!=0) or (len(data_directory)!=0)
+    
+            if len(data_doctors)>0:
+                data=pd.json_normalize(data_doctors)
+                data['doctolib_profile']=True
+                df_final=df_final.append(data)
+    
+            if len(data_directory)>0:
+                data=pd.json_normalize(data_directory)
+                data=data.rename(columns={'zip_code':'zipcode'})
+                data['doctolib_profile']=False
+                data['profile_id']=-1
+                df_final=df_final.append(data)
+    
+            time.sleep(0.5)
     
     return df_final
 
@@ -125,6 +124,6 @@ def save_database(df):
     df.to_sql('doctors', engine, if_exists='replace',index=False)
     
 if __name__=='__main__':
-    get_specialities()
-    df=get_database()
+    list_of_specialities_slug, list_of_specialities = get_specialities()
+    df=get_database(list_of_specialities_slug)
     save_database(df)
